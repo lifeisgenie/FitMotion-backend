e6426ad54d3a8d7372e920b466b044491f897243
lifeisgenie <walker3z@naver.com>
2024-06-17 11:54:55 +0900

    first commit

---
+++ src/main/java/backend/FitMotion/FitMotionApplication.java
@@ -0,0 +1,13 @@
+package backend.FitMotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/FitMotion/FitMotionApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.FitMotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
